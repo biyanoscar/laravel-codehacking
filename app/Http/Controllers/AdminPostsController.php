f070733cae37d3061f09689dccd2fb989286ffc5
biyanoscar <biyanoscar@gmail.com>
2020-11-29 11:28:44 +0700

    able to create post with static category

---
+++ app/Http/Controllers/AdminPostsController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Requests\PostsCreateRequest;
+use App\Photo;
+use App\Post;
+use Illuminate\Support\Facades\Auth;
+
+class AdminPostsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $posts = Post::all();
+        return view('admin.posts.index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(PostsCreateRequest $request)
+    {
+        $input = $request->all();
+        $user = Auth::user();
+
+        $input['user_id'] = $user->id;
+
+        if ($file = $request->file('photo_id')) {
+            $name = time() . $file->getClientOriginalName(); //nama dengan timestamp
+            $file->move('images', $name); // move to folder images
+            $photo = Photo::create(['file' => $name]);
+            $input['photo_id'] = $photo->id; //take id of photo data
+        }
+        // Post::create($input);
+        $user->posts()->create($input);
+
+        return redirect('/admin/posts');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- app/User.php
+++ app/User.php
@@ -48,4 +48,9 @@ class User extends Authenticatable
         }
         return false;
     }
+
+    public function posts()
+    {
+        return $this->hasMany('App\Post');
+    }
 }
